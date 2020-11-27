4fd0f9b7a117c888d4c4ce29c742ed189113da74
Umar Khalid <umar.qur105@gmail.com>
2020-11-27 21:43:11 +0500

    Input field added

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TextInput } from "react-native";
 
 export default function App() {
+	const [originalPrice, setOriginalPrice] = useState();
+	const [discooutPercentage, setDiscountPercentage] = useState();
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
+			<Text style={styles.headerStyles}> Discount Calculator App</Text>
+			<TextInput style={styles.inputStyles} placeholder="Original Price" />
+			<TextInput
+				style={[styles.inputStyles, { marginTop: 5 }]}
+				placeholder="Discount %"
+			/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
+		// backgroundColor: "grey",
+	},
+	headerStyles: {
+		textAlign: "center",
+		backgroundColor: "#027afa",
+		padding: 5,
+		fontSize: 20,
+		fontWeight: "bold",
+		marginTop: 0,
+		marginBottom: 40,
+		color: "#fff",
+	},
+	inputStyles: {
+		borderWidth: 1,
+		borderColor: "grey",
+		width: "50%",
+		padding: 5,
+		margin: "auto",
+		borderRadius: 5,
 	},
 });
